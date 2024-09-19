f229f06a2188a89841342537997cc4a32e5c4d3c
A01 Dev <126718579+a01adem@users.noreply.github.com>
2024-09-19 21:04:50 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
   owners: ['915689279605342218'],
+  mongoUri: 'mongodb+srv://dislist:dislist01@cluster0.2v3bdjy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
 
   discord_auth: {
     clientId: '1279440380701708431',

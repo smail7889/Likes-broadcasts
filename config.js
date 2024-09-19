1e7e109bf47419da36f24dc3a85c52a6cb4e9716
A01 Dev <126718579+a01adem@users.noreply.github.com>
2024-09-19 19:13:57 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  owners: ['915689279605342218'],
+
+  discord_auth: {
+    clientId: '1231273898813952060',
+    clientSecret: '6_A6GqVXjMAJ7WPgFY1I1xq28fYe6z1i',
+    callback: 'https://86648e35-f954-43b2-99e4-5a90041dae74-00-3rptnmyosq6v5.worf.replit.dev/auth/discord/callback',
+  }
+}

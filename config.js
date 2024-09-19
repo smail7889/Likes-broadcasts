25906e32ee966f6aced68662ce766c966bd77523
A01 Dev <126718579+a01adem@users.noreply.github.com>
2024-09-19 19:17:33 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ module.exports = {
   discord_auth: {
     clientId: '1231273898813952060',
     clientSecret: '6_A6GqVXjMAJ7WPgFY1I1xq28fYe6z1i',
-    callback: 'https://86648e35-f954-43b2-99e4-5a90041dae74-00-3rptnmyosq6v5.worf.replit.dev/auth/discord/callback',
+    callback: 'https://like-store-broadcast.onrender.com/auth/discord/callback',
   }
 }

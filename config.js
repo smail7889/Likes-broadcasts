222fcebb7d5df8d9131c53fb69a6d5377cd7f67a
A01 Dev <126718579+a01adem@users.noreply.github.com>
2024-09-20 22:00:27 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ module.exports = {
   mongoUri: 'mongodb+srv://dislist:dislist01@cluster0.2v3bdjy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
 
   discord_auth: {
-    clientId: '1279440380701708431',
-    clientSecret: 'alFK4Bb6DOsHWiEUixlUPfq0mvnsloAs',
+    clientId: '1286793146365251685',
+    clientSecret: '4z4Ejxs-yb4tFHfya6SmIFRjg_ssPi-x',
     callback: 'https://like-store-broadcast.onrender.com/auth/discord/callback',
   }
 }
